f9316d986b4b51b3ff7dae259e4827bfb0e3b298
Grundlefleck <Grundlefleck@c7a0535c-eda6-11de-83d8-6d5adf01d787>
2009-12-23 20:44:30 +0000

    Issue 1:  	 License information is lacking
    Added header with small snippet of license information.
    
    git-svn-id: http://mutability-detector.googlecode.com/svn@15 c7a0535c-eda6-11de-83d8-6d5adf01d787

--- trunk/MutabilityDetector/src/org/mutabilitydetector/IAnalysisSession.java
+++ trunk/MutabilityDetector/src/org/mutabilitydetector/IAnalysisSession.java
@@ -1,3 +1,12 @@
+/*
+ * Mutability Detector
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * Further licensing information for this project can be found in 
+ * 		license/LICENSE.txt
+ */
 package org.mutabilitydetector;
 
 import java.util.Collection;
