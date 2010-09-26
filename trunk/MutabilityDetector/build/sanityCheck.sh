17e152f9d2649016731dcfd963386ff7ac10ffba
Grundlefleck <Grundlefleck@c7a0535c-eda6-11de-83d8-6d5adf01d787>
2010-09-26 15:22:24 +0000

    Dirty script to check that a jar is not fundamentally broken.
    git-svn-id: http://mutability-detector.googlecode.com/svn@92 c7a0535c-eda6-11de-83d8-6d5adf01d787

---
+++ trunk/MutabilityDetector/build/sanityCheck.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Runs an analysis with the built MutabilityDetector.jar for a quick check that nothing is fundamentally broken.
+
+
+java -jar MutabilityDetector.jar -cp MutabilityDetector.jar -verbose
