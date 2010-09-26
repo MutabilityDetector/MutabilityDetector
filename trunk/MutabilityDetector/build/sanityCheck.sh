7b0b15f531712fe9f5db3ff202b52b642dc436bf
Grundlefleck <Grundlefleck@c7a0535c-eda6-11de-83d8-6d5adf01d787>
2010-09-26 23:20:51 +0000

    Analyse rt.jar for the sanity check. Time it as well.
    git-svn-id: http://mutability-detector.googlecode.com/svn@106 c7a0535c-eda6-11de-83d8-6d5adf01d787

--- trunk/MutabilityDetector/build/sanityCheck.sh
+++ trunk/MutabilityDetector/build/sanityCheck.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 # Runs an analysis with the built MutabilityDetector.jar for a quick check that nothing is fundamentally broken.
 
+RT_JAR=$JAVA_HOME/jre/lib/rt.jar
+COMMAND="java -jar MutabilityDetector.jar -cp $RT_JAR"
 
-java -jar MutabilityDetector.jar -cp MutabilityDetector.jar -verbose
+echo "Running command: ${COMMAND}"
+date
+$COMMAND
+date
+exit 0
