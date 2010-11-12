33055ed24c59a7090f09bbd2ee04e54c5cc4856e
Grundlefleck <Grundlefleck@c7a0535c-eda6-11de-83d8-6d5adf01d787>
2010-11-12 22:57:39 +0000

    Tidy up test.
    git-svn-id: http://mutability-detector.googlecode.com/svn@232 c7a0535c-eda6-11de-83d8-6d5adf01d787

--- trunk/MutabilityDetector/trunk/MutabilityDetector/src/test/java/org/mutabilitydetector/benchmarks/StackOverflowInAnalysisTest.java
+++ trunk/MutabilityDetector/trunk/MutabilityDetector/src/test/java/org/mutabilitydetector/benchmarks/StackOverflowInAnalysisTest.java
@@ -1,19 +1,16 @@
-/* 
+/*
  * Mutability Detector
- *
+ * 
  * Copyright 2009 Graham Allan
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  * 
- * 		http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package org.mutabilitydetector.benchmarks;
 
@@ -25,36 +22,30 @@ import org.mutabilitydetector.IAnalysisSession;
 import org.mutabilitydetector.benchmarks.MutableByAssigningAbstractTypeToField.AbstractStringContainer;
 import org.mutabilitydetector.benchmarks.types.EnumType;
 
-
-
 public class StackOverflowInAnalysisTest {
 
-	private IAnalysisSession session = new AnalysisSession(null);
+	private IAnalysisSession session = AnalysisSession.createWithCurrentClassPath();
 
-	@Test
-	public void testInnerClassDoesNotCauseStackOverflowError() throws Exception {
+	@Test public void innerClassDoesNotCauseStackOverflowError() throws Exception {
 		session.isImmutable(ImmutableExample.class.getName());
-		
+
 		session.isImmutable(AbstractStringContainer.class.getName());
 		session.isImmutable(MutableByAssigningAbstractTypeToField.class.getName());
 		session.isImmutable(MutableByAssigningAbstractTypeToField.AbstractStringContainer.class.getName());
 	}
-	
-	@Test
-	public void testVisitingEnumTypeDoesNotCauseStackOverflowError() throws Exception {
+
+	@Test public void visitingEnumTypeDoesNotCauseStackOverflowError() throws Exception {
 		session.isImmutable(EnumType.class.getName());
 	}
-	
-	@Test
-	public void testAnalysingThisTestDoesNotCauseStackOverflowError() throws Exception {
+
+	@Test public void analysingThisTestDoesNotCauseStackOverflowError() throws Exception {
 		session.isImmutable(this.getClass().getName());
 	}
-	
-	@Test
-	public void testAnalysingConstructorClassDoesNotCauseStackOverflow() throws Exception {
+
+	@Test public void analysingConstructorClassDoesNotCauseStackOverflow() throws Exception {
 		// Constructor has a field of type 'Class'
 		// Class has a field of type 'Constructor'
 		session.isImmutable(Constructor.class.getName());
 	}
-	
+
 }
