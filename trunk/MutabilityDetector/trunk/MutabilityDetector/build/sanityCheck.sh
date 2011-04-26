8fb599d7a46d550bbb4d124cfd598c35cf96ef15
grundlefleck <grundlefleck@c7a0535c-eda6-11de-83d8-6d5adf01d787>
2011-04-26 20:55:34 +0000

    Allow passing in jar location to script.
    git-svn-id: http://mutability-detector.googlecode.com/svn@349 c7a0535c-eda6-11de-83d8-6d5adf01d787

--- trunk/MutabilityDetector/trunk/MutabilityDetector/build/sanityCheck.sh
+++ trunk/MutabilityDetector/trunk/MutabilityDetector/build/sanityCheck.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 # Runs an analysis with the built MutabilityDetector.jar for a quick check that nothing is fundamentally broken.
 
+
+MD_JAR=$1
 RT_JAR=$JAVA_HOME/jre/lib/rt.jar
-COMMAND="java -jar MutabilityDetector.jar -cp $RT_JAR"
+COMMAND="java -jar ${MD_JAR} -cp $RT_JAR"
 
 echo "Running command: ${COMMAND}"
 date
