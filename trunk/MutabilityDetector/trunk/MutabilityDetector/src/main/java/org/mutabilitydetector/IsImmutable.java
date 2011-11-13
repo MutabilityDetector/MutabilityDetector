8856527e095f7dfd8bee85518a5c56c6aab343ae
Grundlefleck <Grundlefleck@c7a0535c-eda6-11de-83d8-6d5adf01d787>
2011-11-13 16:10:08 +0000

    Add javadoc to IsImmutable
    git-svn-id: http://mutability-detector.googlecode.com/svn@437 c7a0535c-eda6-11de-83d8-6d5adf01d787

--- trunk/MutabilityDetector/trunk/MutabilityDetector/src/main/java/org/mutabilitydetector/IsImmutable.java
+++ trunk/MutabilityDetector/trunk/MutabilityDetector/src/main/java/org/mutabilitydetector/IsImmutable.java
@@ -1,6 +1,33 @@
 package org.mutabilitydetector;
 
-// TODO:  write javadoc
+/**
+ * The various classifications for instances of classes.
+ * 
+ * @author Grundlefleck at gmail dot com
+ *
+ */
 public enum IsImmutable {
-    IMMUTABLE, EFFECTIVELY_IMMUTABLE, NOT_IMMUTABLE, COULD_NOT_ANALYSE;
+
+    /**
+     * Instances are perfectly immutable, can be safely published without extra synchronisation.
+     */
+    IMMUTABLE,
+
+    /**
+     * Instances are only immutable when safely published. Extra synchronisation is required to publish effectively
+     * immutable objects safely.
+     */
+    EFFECTIVELY_IMMUTABLE,
+
+    /**
+     * Instances are not immutable, do not cache and/or share instances, do not publish to multiple threads, do not use
+     * as keys in {@link HashMap}s.
+     */
+    NOT_IMMUTABLE,
+
+    /**
+     * Instances can be any of {@link #IMMUTABLE}/{@link IsImmutable#IMMUTABLE}/{@link #NOT_IMMUTABLE}, however, an
+     * internal error has occurred during analysis, and no result is meaningful.
+     */
+    COULD_NOT_ANALYSE;
 }
