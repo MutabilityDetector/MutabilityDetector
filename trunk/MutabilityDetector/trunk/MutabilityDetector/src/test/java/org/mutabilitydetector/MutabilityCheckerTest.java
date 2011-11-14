4c6a2329c5bcb74808921200c170acf05c0a163e
Grundlefleck <Grundlefleck@c7a0535c-eda6-11de-83d8-6d5adf01d787>
2011-11-14 20:23:49 +0000

    Fix up out of date license information.
    git-svn-id: http://mutability-detector.googlecode.com/svn@445 c7a0535c-eda6-11de-83d8-6d5adf01d787

--- trunk/MutabilityDetector/trunk/MutabilityDetector/src/test/java/org/mutabilitydetector/MutabilityCheckerTest.java
+++ trunk/MutabilityDetector/trunk/MutabilityDetector/src/test/java/org/mutabilitydetector/MutabilityCheckerTest.java
@@ -1,19 +1,18 @@
 /*
- * Mutability Detector
- * 
- * Copyright 2009 Graham Allan
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ *    Copyright (c) 2008-2011 Graham Allan
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
  */
 package org.mutabilitydetector;
 
@@ -43,14 +42,6 @@ import org.mutabilitydetector.benchmarks.mutabletofield.MutableByAssigningInterf
 import org.mutabilitydetector.benchmarks.settermethod.MutableByHavingSetterMethod;
 import org.mutabilitydetector.benchmarks.types.EnumType;
 
-/**
- * This test acts as an overall progress checker as well as a general acceptance of the tool so far. There are several
- * classes used as micro benchmarks to tell if the tool is correct. Once the checker can correctly assess these classes
- * the tool is correct for our definition.
- * 
- * @author Graham Allan (grundlefleck@gmail.com)
- * 
- */
 public class MutabilityCheckerTest {
 
     @Test
