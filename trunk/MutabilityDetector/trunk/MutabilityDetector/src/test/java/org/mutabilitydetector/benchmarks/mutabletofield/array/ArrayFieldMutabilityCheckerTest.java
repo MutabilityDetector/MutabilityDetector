42237f16ed3fca93fa864b46bd62c846df4dd8bc
grundlefleck <grundlefleck@c7a0535c-eda6-11de-83d8-6d5adf01d787>
2012-01-03 20:49:36 +0000

    Add test cases for upcoming array field analysis.
    googlecode issue 18
    
    git-svn-id: http://mutability-detector.googlecode.com/svn@491 c7a0535c-eda6-11de-83d8-6d5adf01d787

--- trunk/MutabilityDetector/trunk/MutabilityDetector/src/test/java/org/mutabilitydetector/benchmarks/mutabletofield/array/ArrayFieldMutabilityCheckerTest.java
+++ trunk/MutabilityDetector/trunk/MutabilityDetector/src/test/java/org/mutabilitydetector/benchmarks/mutabletofield/array/ArrayFieldMutabilityCheckerTest.java
@@ -19,18 +19,28 @@ package org.mutabilitydetector.benchmarks.mutabletofield.array;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.mutabilitydetector.ImmutableAssert.assertImmutable;
+import static org.mutabilitydetector.ImmutableAssert.assertIsImmutable;
 import static org.mutabilitydetector.ImmutableAssert.assertNotImmutable;
+import static org.mutabilitydetector.IsImmutable.IMMUTABLE;
+import static org.mutabilitydetector.IsImmutable.NOT_IMMUTABLE;
 import static org.mutabilitydetector.TestMatchers.hasReasons;
 import static org.mutabilitydetector.TestUtil.runChecker;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 import org.mutabilitydetector.AnalysisResult;
+import org.mutabilitydetector.AnalysisResultTheory;
 import org.mutabilitydetector.benchmarks.types.ClassWithAllPrimitives;
 import org.mutabilitydetector.checkers.ArrayFieldMutabilityChecker;
 import org.mutabilitydetector.checkers.AsmMutabilityChecker;
 import org.mutabilitydetector.locations.FieldLocation;
 
+@RunWith(Theories.class)
 public class ArrayFieldMutabilityCheckerTest {
 
     private AsmMutabilityChecker checker;
@@ -63,6 +73,23 @@ public class ArrayFieldMutabilityCheckerTest {
 
         assertThat(sourceLocation.typeName(), is(ClassWithAllPrimitives.Array.class.getName()));
         assertThat(sourceLocation.fieldName(), is("anArray"));
+    }
+
+    @DataPoints
+    public static final AnalysisResultTheory[] classes = new AnalysisResultTheory[] {
+            AnalysisResultTheory.of(ImmutableButHasUnmodifiedArrayAsField.class, IMMUTABLE),
+            AnalysisResultTheory.of(ImmutableByDefensivelyCopyingAndGuardingArray.class, IMMUTABLE),
+            AnalysisResultTheory.of(ImmutableWhenArrayFieldIsStatic.class, IMMUTABLE),
+            AnalysisResultTheory.of(MutableAsElementsOfArrayAreMutableAndPublished.class, NOT_IMMUTABLE),
+            AnalysisResultTheory.of(MutableByAssigningPrimitiveArrayToField.class, NOT_IMMUTABLE),
+            AnalysisResultTheory.of(MutableByHavingArrayTypeAsField.class, NOT_IMMUTABLE),
+            AnalysisResultTheory.of(MutableByPublishingArray.class, NOT_IMMUTABLE), };
+
+
+    @Ignore
+    @Theory
+    public void correctlyAnalyses(AnalysisResultTheory expected) throws Exception {
+        assertIsImmutable(expected.expected, runChecker(checker, expected.clazz));
     }
 
 }
