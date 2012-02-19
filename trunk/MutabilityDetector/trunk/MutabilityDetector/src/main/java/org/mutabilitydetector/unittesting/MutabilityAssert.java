b9f061cc8f0c2c22d14d346e9648a05d4bf51dd8
grundlefleck <grundlefleck@c7a0535c-eda6-11de-83d8-6d5adf01d787>
2012-02-19 14:56:32 +0000

    Improve the main javadocs for MutabilityAssert by including more detail on the out-of-the-box matchers.
    git-svn-id: http://mutability-detector.googlecode.com/svn@517 c7a0535c-eda6-11de-83d8-6d5adf01d787

--- trunk/MutabilityDetector/trunk/MutabilityDetector/src/main/java/org/mutabilitydetector/unittesting/MutabilityAssert.java
+++ trunk/MutabilityDetector/trunk/MutabilityDetector/src/main/java/org/mutabilitydetector/unittesting/MutabilityAssert.java
@@ -23,11 +23,13 @@ import static org.mutabilitydetector.unittesting.matchers.reasons.WithAllowedRea
 import static org.mutabilitydetector.unittesting.matchers.reasons.WithAllowedReasonsMatcher.withNoAllowedReasons;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.hamcrest.Matcher;
 import org.mutabilitydetector.AnalysisResult;
 import org.mutabilitydetector.IsImmutable;
+import org.mutabilitydetector.MutabilityReason;
 import org.mutabilitydetector.MutableReasonDetail;
 import org.mutabilitydetector.unittesting.internal.AnalysisSessionHolder;
 import org.mutabilitydetector.unittesting.internal.AssertionReporter;
@@ -51,9 +53,16 @@ import org.mutabilitydetector.unittesting.matchers.reasons.WithAllowedReasonsMat
  * <li><a href="#FirstTestCase">Your first test case.</a></li>
  * <li><a href="#ConfiguringTheAssertion">A more specific assertion</a></li>
  * <li><a href="#AllowingAReason">Allowing a reason for mutability</a></li>
- *  <ul>
- *  <li><a href="#OutOfTheBox">Out-of-the-box Allowed Reasons</a></li>
- *  </ul>
+ * <ul>
+ * <li><a href="#OutOfTheBox">Out-of-the-box Allowed Reasons</a>
+ * <ul>
+ * <li><a href="AllowingAbstractImmutable">Abstract immutable implementations</a></li>
+ * <li><a href="AllowingAbstractImmutableFields">Assigning abstract immutable fields</a></li>
+ * <li><a href="AllowingStringFields">Bypassing The String Problem</a></li>
+ * </ul>
+ * </li>
+ * <li><a href="WritingAnAllowedReason">Writing your own allowed reason</a></li>
+ * </ul>
  * </li>
  * </ol>
  * 
@@ -76,49 +85,57 @@ import org.mutabilitydetector.unittesting.matchers.reasons.WithAllowedReasonsMat
  * 
  * <h3 id="FirstTestCase">Your first test case.</h3>
  * 
- * The most simple assertion you can make will look something like this: 
- * <pre><code>
+ * The most simple assertion you can make will look something like this:
+ * 
+ * <pre>
+ * <code>
  * import static org.mutabilitydetector.unittesting.MutabilityAssert.assertImmutable;
  * 
  * &#064;Test public void checkMyClassIsImmutable() {
  *     assertImmutable(MyClass.class); 
  * }
- * </code></pre>
+ * </code>
+ * </pre>
+ * 
  * <p>
- * This assertion will trigger an analysis of
- * <code>MyClass</code>, passing if found to be immutable, failing if found to be mutable.
+ * This assertion will trigger an analysis of <code>MyClass</code>, passing if found to be immutable, failing if found
+ * to be mutable.
  * </p>
  * 
  * 
- * <h3 id="ConfiguringTheAssertion">Configuring the assertion</h3> 
+ * <h3 id="ConfiguringTheAssertion">Configuring the assertion</h3>
  * <p>
- * The method used above is a shortcut for more
- * expressive forms of the assertion, and does not allow any further configuration. An equivalent assertion is:
+ * The method used above is a shortcut for more expressive forms of the assertion, and does not allow any further
+ * configuration. An equivalent assertion is:
  * </p>
  * 
- * <pre><code>
+ * <pre>
+ * <code>
  * import static org.mutabilitydetector.unittesting.MutabilityAssert.assertInstancesOf;
  * import static org.mutabilitydetector.unittesting.MutabilityMatchers.areImmutable;
  * 
  * &#064;Test public void checkMyClassIsImmutable() {
  *     assertInstancesOf(MyClass.class, areImmutable()); 
- * }</code></pre> 
+ * }</code>
+ * </pre>
  * 
  * 
- * This is the form that can be used for extra configuration of the assertion. Let's take a look at an
- * assertion that is configured differently. Consider a class which is immutable, except for fields not being declared
- * <code>final</code>. According to <a href="http://jcip.net/">Java Concurrency In Practice</a>, instances of classes
- * like this, as long as they are <i>safely publised</i> are still considered <i>effectively immutable</i>. Please note
- * however, Mutability Detector does not check that objects are safely published. <br />
+ * This is the form that can be used for extra configuration of the assertion. Let's take a look at an assertion that is
+ * configured differently. Consider a class which is immutable, except for fields not being declared <code>final</code>.
+ * According to <a href="http://jcip.net/">Java Concurrency In Practice</a>, instances of classes like this, as long as
+ * they are <i>safely publised</i> are still considered <i>effectively immutable</i>. Please note however, Mutability
+ * Detector does not check that objects are safely published. <br />
  * To represent this in a unit test, the assertion would like this:
  * 
- * <pre><code>
+ * <pre>
+ * <code>
  * import static org.mutabilitydetector.unittesting.MutabilityAssert.assertInstancesOf;
  * import static org.mutabilitydetector.unittesting.MutabilityMatchers.areEffectivelyImmutable;
  * 
  * &#064;Test public void checkMyClassIsImmutable() {
  *     assertInstancesOf(MyClassWhereTheFieldsAreNotFinal.class, areEffectivelyImmutable());
- * }</code></pre>
+ * }</code>
+ * </pre>
  * 
  * See also:
  * <ul>
@@ -139,13 +156,15 @@ import org.mutabilitydetector.unittesting.matchers.reasons.WithAllowedReasonsMat
  * There can also be cases where your class is found to be mutable, but you know for your scenario that it's an
  * acceptable reason. Consider the following class:
  * 
- * <pre><code>public abstract class AbstractIntHolder {
+ * <pre>
+ * <code>public abstract class AbstractIntHolder {
  *   private final int intField;
  *   
  *   public AbstractIntHolder(int intToStore) {
  *     this.intField = intToStore;
  *   }
- * }</code></pre>
+ * }</code>
+ * </pre>
  * 
  * <p>
  * In this case, if you assert <code>AbstractIntHolder</code> is immutable, the test will fail. This is because
@@ -160,7 +179,8 @@ import org.mutabilitydetector.unittesting.matchers.reasons.WithAllowedReasonsMat
  * <code>AbstractIntHolder</code> could look like this:
  * </p>
  * 
- * <pre><code>
+ * <pre>
+ * <code>
  * import static org.mutabilitydetector.unittesting.MutabilityAssert.assertInstancesOf;
  * import static org.mutabilitydetector.unittesting.MutabilityMatchers.areEffectivelyImmutable;
  * import static org.mutabilitydetector.unittesting.AllowedReason.allowingForSubclassing;
@@ -168,22 +188,74 @@ import org.mutabilitydetector.unittesting.matchers.reasons.WithAllowedReasonsMat
  * &#064;Test public void checkMyClassIsImmutable() {
  *     assertInstancesOf(AbstractIntHolder.class, areImmutable(), allowingForSubclassing());
  * }
- * </code></pre>
+ * </code>
+ * </pre>
  * 
  * This will allow your test to pass, but fail for any other reasons that are introduced, e.g. if someone adds a setter
  * method.
  * 
  * <p>
  * Similar to the <code>Matcher&ltAnalysisResult&gt;</code> parameter, the allowed reason parameter of
- * {@link #assertInstancesOf(Class, Matcher, Matcher)} is a <code>Matcher&lt;{@link MutableReasonDetail}&gt;</code>. Mutability
- * Detector will provide only a few out-of-the-box implementations for this, which are unlikely to cover each scenario
- * where you want to permit a certain aspect of mutability.
+ * {@link #assertInstancesOf(Class, Matcher, Matcher)} is a <code>Matcher&lt;{@link MutableReasonDetail}&gt;</code>.
+ * Mutability Detector will provide only a few out-of-the-box implementations for this, which are unlikely to cover each
+ * scenario where you want to permit a certain aspect of mutability.
  * </p>
+ * <h3 id="OutOfTheBox">Out-of-the-box allowed reasons</h3>
  * <p>
- * For a mutable class to pass the test, each {@link MutableReasonDetail} must be matched by at least one allowed reason.
+ * <h4 id="AllowingAbstractImmutable">Abstract class with immutable implementation</h4>
+ * It can be useful to write an abstract class, designed for extension, which is immutable. To ensure that a concrete
+ * class B, extending abstract class A is immutable, it is necessary to test that both <code>A</code> and <code>B</code>
+ * are immutable. However, if you write the assertion <code>assertImmutable(A.class);</code>, it will fail, as it can be
+ * subclassed (see {@link MutabilityReason#CAN_BE_SUBCLASSED}). To specifically allow this, use the allowed reason:
+ * {@link AllowedReason#allowingForSubclassing()} <br>
+ * <br>
+ * For example:<br>
+ * <code>assertInstancesOf(A.class, areImmutable(), allowingForSubclassing());</code>
+ * 
+ * <h4 id="AllowingAbstractImmutableFields">Depending on other classes being immutable</h4>
+ * Consider the following code: <code>
+ * <pre>
+ * public final class MyImmutable {
+ *  public final ShouldAlsoBeImmutable field;
+ *  
+ *  public MyImmutable(ShouldAlsoBeImmutable dependsOnThisBeingImmutable) {
+ *      this.field = dependsOnThisBeingImmutable;
+ *  }
+ * }
+ * </pre>
+ * <code>
+ * 
+ * If <code>ShouldAlsoBeImmutable</code> is not a concrete class (an <code>interface</code> or <code>abstract</code>
+ * class), <code>assertImmutable(MyImmutable.class);</code> will fail, as there's no guarantee that the runtime
+ * implementation of <code>ShouldBeImmutable</code> is actually immutable. A common example is taking a parameter of
+ * <code>java.util.List</code>, where you require that it is an immutable implementation, e.g: a copy created with
+ * {@link Collections#unmodifiableList(List)}. For this scenario, use {@link AllowedReason#provided(Class)}.
+ * 
+ * To make the above example pass, use an allowed reason like so:<br>
+ * <br>
+ * 
+ * 
+ * <pre>
+ * <code>
+ * assertInstancesOf(MyImmutable.class, areImmutable(),
+ *                   AllowedReason.provided(ShouldAlsoBeImmutable.class).isAlsoImmutable()); 
+ * </code>
+ * </pre>
+ * 
+ * <h4 id="AllowingStringFields">Using {@link String} fields in immutable objects</h4>
+ * Until <a href="http://code.google.com/p/mutability-detector/issues/detail?id=3">The String Problem</a> is resolved,
+ * using <code>provided(String.class).isAlsoImmutable()</code> is the only practical solution for when your immutable
+ * classes have {@link String} fields. This sucks, I know :-(
+ * 
  * </p>
  * 
- * <h3 id="OutOfTheBox">
+ * <h3 id="WritingAnAllowedReason">Writing your own allowed reasons</h3>
+ * <p>
+ * If none of the out-of-the-box allowed reasons suit your needs, it is possible to supply your own implementation. The
+ * allowed reason in the signature of {@link MutabilityAssert#assertInstancesOf(Class, Matcher, Matcher)} is a
+ * Hamcrest <code>Matcher&lt;{@link MutableReasonDetail}&gt;</code>. For a mutable class to pass the test, each
+ * {@link MutableReasonDetail} of the {@link AnalysisResult} (provided by Mutability Detector) must be matched by at least one allowed reason.
+ * </p>
  * 
  * @author Graham Allan / Grundlefleck at gmail dot com
  * 
