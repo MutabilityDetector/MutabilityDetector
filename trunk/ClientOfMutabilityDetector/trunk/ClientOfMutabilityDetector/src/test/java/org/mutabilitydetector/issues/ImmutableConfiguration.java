c661dd61cc5d8adb3353f94dbf16ce3149bb828d
grundlefleck <grundlefleck@c7a0535c-eda6-11de-83d8-6d5adf01d787>
2012-03-07 21:39:29 +0000

    Add a test case for googlecode issue 25.
    git-svn-id: http://mutability-detector.googlecode.com/svn@555 c7a0535c-eda6-11de-83d8-6d5adf01d787

---
+++ trunk/ClientOfMutabilityDetector/trunk/ClientOfMutabilityDetector/src/test/java/org/mutabilitydetector/issues/ImmutableConfiguration.java
@@ -0,0 +1,123 @@
+package org.mutabilitydetector.issues;
+
+import static java.util.Arrays.asList;
+import static org.mutabilitydetector.MutabilityReason.ABSTRACT_TYPE_TO_FIELD;
+import static org.mutabilitydetector.issues.ImmutableConfiguration.MutabilityTest.AssumeCopiedIntoUnmodifiableCollection.assuming;
+import static org.mutabilitydetector.locations.Dotted.dotted;
+import static org.mutabilitydetector.unittesting.AllowedReason.provided;
+import static org.mutabilitydetector.unittesting.MutabilityAssert.assertInstancesOf;
+import static org.mutabilitydetector.unittesting.MutabilityMatchers.areImmutable;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.junit.Test;
+import org.mutabilitydetector.MutableReasonDetail;
+import org.mutabilitydetector.locations.Dotted;
+import org.mutabilitydetector.locations.FieldLocation;
+import org.mutabilitydetector.repackaged.org.hamcrest.TypeSafeDiagnosingMatcher;
+
+@SuppressWarnings({ "unchecked", "rawtypes" })
+public final class ImmutableConfiguration {
+
+    @SuppressWarnings("unused")
+    private final Map<String, Object> propMap;
+
+    public ImmutableConfiguration(AbstractConfiguration origConfiguration) {
+
+        Map store = new HashMap();
+        // we need the variables interpolated, which doesn't come at the getProperty level
+        Configuration interpolated = origConfiguration.interpolatedConfiguration();
+        final Iterator keyIter = interpolated.getKeys();
+        while (keyIter.hasNext()) {
+            String key = (String) keyIter.next();
+            store.put(key, interpolated.getProperty(key));
+        }
+        this.propMap = Collections.unmodifiableMap(store);
+    }
+
+    public static interface Configuration {
+        Iterator getKeys();
+        Object getProperty(String key);
+    }
+
+    public static abstract class AbstractConfiguration {
+        public abstract Configuration interpolatedConfiguration();
+    }
+    
+    public static class MutabilityTest {
+        @Test
+        public void immutableConfiguration_assumeAnyMapIsImmutable() throws Exception {
+            assertInstancesOf(ImmutableConfiguration.class, 
+                              areImmutable(),
+                              provided(Map.class).isAlsoImmutable());
+        }
+        
+        @Test
+        public void immutableConfiguration_assumeACopyFromUnmodifiableCollection() throws Exception {
+            assertInstancesOf(ImmutableConfiguration.class, 
+                              areImmutable(),
+                              assuming("propMap").hadUnmodifiableCollectionAssignedToIt());
+        }
+        
+        public static class AssumeCopiedIntoUnmodifiableCollection extends TypeSafeDiagnosingMatcher<MutableReasonDetail> {
+
+            private final String fieldName;
+
+            private static final List<Dotted> unmodifiableTypes = asList(dotted("java.util.List"), 
+                                                                            dotted("java.util.Map"), 
+                                                                            dotted("java.util.Set"),
+                                                                            dotted("java.util.Collection"),
+                                                                            dotted("java.util.SortedSet"),
+                                                                            dotted("java.util.SortedMap"));
+
+            private static class Assuming {
+                private final String fieldName;
+                public Assuming(String fieldName) {
+                    this.fieldName = fieldName;
+                }
+                public Matcher<MutableReasonDetail> hadUnmodifiableCollectionAssignedToIt() {
+                    return new AssumeCopiedIntoUnmodifiableCollection(fieldName);
+                }
+                
+            }
+
+            public static Assuming assuming(String fieldName) {
+                return new Assuming(fieldName);
+            }
+            
+            public AssumeCopiedIntoUnmodifiableCollection(String fieldName) {
+                this.fieldName = fieldName;
+                
+            }
+            
+            @Override
+            public void describeTo(Description description) { }
+
+            @Override
+            protected boolean matchesSafely(MutableReasonDetail reasonDetail, Description description) {
+                if (reasonDetail.reason().isOneOf(ABSTRACT_TYPE_TO_FIELD)) {
+                    String potentiallyAbstractField = ((FieldLocation) reasonDetail.codeLocation()).fieldName();
+                    Dotted assignedType = sniffOutAssignedTypeFromMessage(reasonDetail.message());
+                    if (potentiallyAbstractField.equals(fieldName) && unmodifiableTypes.contains(assignedType)) {
+                        return true;
+                    }
+                }
+                
+                return false;
+            }
+
+
+            private Dotted sniffOutAssignedTypeFromMessage(String message) {
+                return dotted(message.substring(message.lastIndexOf("(") + 1, message.lastIndexOf(")")));
+            }
+
+        }
+
+    }
+}
