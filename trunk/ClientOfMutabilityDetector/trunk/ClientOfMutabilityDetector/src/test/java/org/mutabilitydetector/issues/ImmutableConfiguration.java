d14ac0887e9dbe3c78eaa48b3c7a584235698f71
grundlefleck <grundlefleck@c7a0535c-eda6-11de-83d8-6d5adf01d787>
2012-06-04 20:48:36 +0000

    Try to jiggle the types to make cloudbees happy.
    git-svn-id: http://mutability-detector.googlecode.com/svn@589 c7a0535c-eda6-11de-83d8-6d5adf01d787

--- trunk/ClientOfMutabilityDetector/trunk/ClientOfMutabilityDetector/src/test/java/org/mutabilitydetector/issues/ImmutableConfiguration.java
+++ trunk/ClientOfMutabilityDetector/trunk/ClientOfMutabilityDetector/src/test/java/org/mutabilitydetector/issues/ImmutableConfiguration.java
@@ -1,9 +1,6 @@
 package org.mutabilitydetector.issues;
 
-import static java.util.Arrays.asList;
-import static org.mutabilitydetector.MutabilityReason.ABSTRACT_TYPE_TO_FIELD;
-import static org.mutabilitydetector.issues.ImmutableConfiguration.MutabilityTest.AssumeCopiedIntoUnmodifiable.assuming;
-import static org.mutabilitydetector.locations.Dotted.dotted;
+import static org.mutabilitydetector.issues.AssumeCopiedIntoUnmodifiable.assuming;
 import static org.mutabilitydetector.unittesting.AllowedReason.provided;
 import static org.mutabilitydetector.unittesting.MutabilityAssert.assertInstancesOf;
 import static org.mutabilitydetector.unittesting.MutabilityMatchers.areImmutable;
@@ -11,16 +8,9 @@ import static org.mutabilitydetector.unittesting.MutabilityMatchers.areImmutable
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
-import org.hamcrest.BaseMatcher;
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
 import org.junit.Test;
-import org.mutabilitydetector.MutableReasonDetail;
-import org.mutabilitydetector.locations.Dotted;
-import org.mutabilitydetector.locations.FieldLocation;
 
 @SuppressWarnings({ "unchecked", "rawtypes" })
 public final class ImmutableConfiguration {
@@ -66,61 +56,6 @@ public final class ImmutableConfiguration {
             assertInstancesOf(ImmutableConfiguration.class, 
                               areImmutable(),
                               assuming("propMap").hasCollectionsUnmodifiableTypeAssignedToIt());
-        }
-        
-        public static class AssumeCopiedIntoUnmodifiable extends BaseMatcher<MutableReasonDetail> {
-
-            private static class Assuming {
-                private final String fieldName;
-                public Assuming(String fieldName) {
-                    this.fieldName = fieldName;
-                }
-                public Matcher<MutableReasonDetail> hasCollectionsUnmodifiableTypeAssignedToIt() {
-                    return new AssumeCopiedIntoUnmodifiable(fieldName);
-                }
-                
-            }
-
-            public static Assuming assuming(String fieldName) {
-                return new Assuming(fieldName);
-            }
-
-            private static final List<Dotted> unmodifiableTypes = asList(dotted("java.util.List"), 
-                                                                         dotted("java.util.Map"), 
-                                                                         dotted("java.util.Set"),
-                                                                         dotted("java.util.Collection"),
-                                                                         dotted("java.util.SortedSet"),
-                                                                         dotted("java.util.SortedMap"));
-
-            private final String fieldName;
-            
-            public AssumeCopiedIntoUnmodifiable(String fieldName) {
-                this.fieldName = fieldName;
-            }
-            
-            @Override
-            public void describeTo(Description description) { }
-
-
-            private Dotted sniffOutAssignedTypeFromMessage(String message) {
-                return dotted(message.substring(message.lastIndexOf("(") + 1, message.lastIndexOf(")")));
-            }
-
-
-            @Override
-            public boolean matches(Object arg0) {
-                MutableReasonDetail reasonDetail = (MutableReasonDetail) arg0;
-                if (reasonDetail.reason().isOneOf(ABSTRACT_TYPE_TO_FIELD)) {
-                    String potentiallyAbstractField = ((FieldLocation) reasonDetail.codeLocation()).fieldName();
-                    Dotted assignedType = sniffOutAssignedTypeFromMessage(reasonDetail.message());
-                    if (potentiallyAbstractField.equals(fieldName) && unmodifiableTypes.contains(assignedType)) {
-                        return true;
-                    }
-                }
-                
-                return false;
-            }
-
         }
 
     }
