4567076759b64405836fbdbb1b69bc4e991d512d
grundlefleck <grundlefleck@c7a0535c-eda6-11de-83d8-6d5adf01d787>
2012-10-21 19:22:03 +0000

    Try to make example more readable.
    git-svn-id: http://mutability-detector.googlecode.com/svn@644 c7a0535c-eda6-11de-83d8-6d5adf01d787

--- trunk/ClientOfMutabilityDetector/trunk/ClientOfMutabilityDetector/src/main/java/org/mutabilitydetector/runtime/RuntimeAnalysisExample.java
+++ trunk/ClientOfMutabilityDetector/trunk/ClientOfMutabilityDetector/src/main/java/org/mutabilitydetector/runtime/RuntimeAnalysisExample.java
@@ -11,12 +11,17 @@ public final class RuntimeAnalysisExample {
     private final AnalysisSession analysisSession = ThreadUnsafeAnalysisSession.createWithCurrentClassPath();
 
     private void run(Object requiredToBeImmutable) {
-        RequestedAnalysis requestedAnalysis = analysisSession.resultFor(Dotted.fromClass(requiredToBeImmutable.getClass()));
+        Dotted runtimeClassOfGivenObject = Dotted.fromClass(requiredToBeImmutable.getClass());
+		RequestedAnalysis requestedAnalysis = analysisSession.resultFor(runtimeClassOfGivenObject);
         AnalysisResult result = requestedAnalysis.result;
+        
         if(result.isImmutable.equals(IsImmutable.IMMUTABLE)) {
-            // rest safe in the knowledge the class is immutable, share across threads with joyful abandon
+            // rest safe in the knowledge the class is 
+        	// immutable, share across threads with joyful abandon
         } else if(result.isImmutable.equals(IsImmutable.NOT_IMMUTABLE)) {
-            // be careful here: make defensive copies, don't publish the reference, read Java Concurrency In Practice right away!
+            // be careful here: make defensive copies, 
+        	// don't publish the reference, 
+        	// read Java Concurrency In Practice right away!
         }
     }
     
