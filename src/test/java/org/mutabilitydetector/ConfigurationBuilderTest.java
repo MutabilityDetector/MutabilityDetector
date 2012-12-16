910cca907381a0378b5db60de617bd42a100185c
Grundlefleck <grundlefleck@gmail.com>
2012-12-16 13:08:40 +0000

    Add more data to the configurations to show merging behaviour.

--- src/test/java/org/mutabilitydetector/ConfigurationBuilderTest.java
+++ src/test/java/org/mutabilitydetector/ConfigurationBuilderTest.java
@@ -3,10 +3,15 @@ package org.mutabilitydetector;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.is;
 import static org.mutabilitydetector.AnalysisResult.analysisResult;
+import static org.mutabilitydetector.AnalysisResult.definitelyImmutable;
 import static org.mutabilitydetector.locations.Dotted.dotted;
 
+import java.util.Map;
+
 import org.junit.Test;
+import org.mutabilitydetector.locations.Dotted;
 
 public class ConfigurationBuilderTest {
 
@@ -14,7 +19,7 @@ public class ConfigurationBuilderTest {
     public void canMergeResultsFromExistingConfiguration() throws Exception {
         final Configuration existing = new ConfigurationBuilder() {
             @Override public void configure() {
-                hardcodeResult(AnalysisResult.definitelyImmutable("hardcoded.in.other.Configuration"));
+                hardcodeResult(definitelyImmutable("hardcoded.in.other.Configuration"));
             }
         }.build();
         
@@ -31,22 +36,29 @@ public class ConfigurationBuilderTest {
     public void mergeReplacesExistingHardcodedResultForClassWithCurrentHardcodedResult() throws Exception {
         final Configuration existing = new ConfigurationBuilder() {
             @Override public void configure() {
-                hardcodeResult(AnalysisResult.definitelyImmutable("hardcoded.in.both.Configurations"));
+                hardcodeResult(definitelyImmutable("hardcoded.in.both.Configurations"));
+                hardcodeResult(definitelyImmutable("only.in.existing.Configuration"));
             }
         }.build();
         
         final AnalysisResult resultInCurrentConfig = analysisResult("hardcoded.in.both.Configurations", 
                                                               IsImmutable.NOT_IMMUTABLE, 
                                                               TestUtil.unusedMutableReasonDetail());
+        
         final Configuration current = new ConfigurationBuilder() {
             @Override public void configure() {
                 hardcodeResult(resultInCurrentConfig);
+                hardcodeResult(definitelyImmutable("only.in.current.Configuration"));
 
                 mergeHardcodedResultsFrom(existing);
             }
         }.build();
         
-        assertThat(current.hardcodedResults(), hasEntry(dotted("hardcoded.in.both.Configurations"),
-                                                        resultInCurrentConfig));
+        Map<Dotted, AnalysisResult> hardcodedResults = current.hardcodedResults();
+        
+        assertThat(hardcodedResults.size(), is(3));
+        assertThat(hardcodedResults, hasEntry(dotted("hardcoded.in.both.Configurations"), resultInCurrentConfig));
+        assertThat(hardcodedResults, hasEntry(dotted("only.in.existing.Configuration"), definitelyImmutable("only.in.existing.Configuration")));
+        assertThat(hardcodedResults, hasEntry(dotted("only.in.current.Configuration"), definitelyImmutable("only.in.current.Configuration")));
     }
 }
