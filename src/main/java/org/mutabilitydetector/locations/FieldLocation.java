37709cb525cfd3da7b8a27f954044e9a8bb4a4fc
Graham Allan <grundlefleck@gmail.com>
2014-08-30 18:28:41 +0100

    Make constructor private.

--- src/main/java/org/mutabilitydetector/locations/FieldLocation.java
+++ src/main/java/org/mutabilitydetector/locations/FieldLocation.java
@@ -9,9 +9,9 @@ package org.mutabilitydetector.locations;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,7 +31,7 @@ public final class FieldLocation implements CodeLocation<FieldLocation> {
     private final @Nonnull String fieldName;
     private final @Nonnull ClassLocation ownerOfField;
 
-    public FieldLocation(String fieldName, ClassLocation ownerOfField) {
+    private FieldLocation(String fieldName, ClassLocation ownerOfField) {
         this.fieldName = fieldName;
         this.ownerOfField = ownerOfField;
     }
